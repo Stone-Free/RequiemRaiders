df55ec193ac3c11d14b6749f31fe617624f79866
Zayvion Worley <59545638+Stone-Free@users.noreply.github.com>
2024-08-14 11:23:03 -0400

    Update EntityAIRaiderWitch.java

--- src/main/java/com/gendeathrow/pmobs/entity/ai/EntityAIRaiderWitch.java
+++ src/main/java/com/gendeathrow/pmobs/entity/ai/EntityAIRaiderWitch.java
@@ -68,37 +68,6 @@ public class EntityAIRaiderWitch  extends EntityAIBase{
 	   return raider.getRNG().nextInt(20) - 10;
    }
    
-   
-   void createBarrier(int x0, int y0, int radius) {
-       int x = radius;
-       int y = 0;
-       int err = 0;
-
-       while (x >= y)
-       {
-           //putpixel(x0 + x, y0 + y);
-           //putpixel(x0 + y, y0 + x);
-           //putpixel(x0 - y, y0 + x);
-           //putpixel(x0 - x, y0 + y);
-           //putpixel(x0 - x, y0 - y);
-           //putpixel(x0 - y, y0 - x);
-           //putpixel(x0 + y, y0 - x);
-           //putpixel(x0 + x, y0 - y);
-
-           if (err <= 0)
-           {
-               y += 1;
-               err += 2*y + 1;
-           }
-           if (err > 0)
-           {
-               x -= 1;
-               err -= 2*x + 1;
-           }
-       }
-	   
-   }
-   
 
   
 }
